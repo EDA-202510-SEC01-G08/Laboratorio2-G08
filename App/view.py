e4548d13ab8d80450faa8a48d92c17b7da2935a0
juanccancelado <j.canceladod@uniandes.edu.co>
2025-02-06 21:42:50 -0500

    cambios del estudiante 1

--- App/view.py
+++ App/view.py
@@ -52,13 +52,12 @@ def print_menu():
     """
     Imprime el menú de opciones en consola para el usuario
     """
-    print("Opciones:")
-    print("1- Cargar Libros")
-    print("2- Cargar Tags")
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    # Agregue la opción 3 para cargar los tags de los libros.
-    # Pueede guiarse de las opciones 1 y 2.
-    print("0- Salir")
+    pprint("Opciones:")
+    print("1-Cargar Libros")
+    print("2-Cargar Tags")
+    # TODO: Mods de Est-1, Est-2 y Est-3 en Lab 2
+    print("3-Cargar Tags de Libros")
+    print("0-Salir")
 
 
 def load_books(app):
@@ -96,7 +95,8 @@ def load_books_tags(app):
     :type app: logic
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    book_tags =logic.load_books_tags(app,"GoodReads/book_tags-small.csv")
+    return book_tags 
 
 
 def first_book(app):
@@ -104,7 +104,8 @@ def first_book(app):
     Devuelve el primer libro cargado en el conjunto de libros
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    first =logic.first_book(app)
+    return first
 
 
 def last_book(app):
@@ -138,6 +139,8 @@ def main():
             print("Total de libros cargados: " + str(books) + "\n")
 
             # TODO: Mods de Est-1 en el Lab 2
+            first =first_book(app)
+            print("Primer libro cargado:\n" +str(first) +"\n")
             first = None
 
             # TODO: Mods de Est-2 en el Lab 2
